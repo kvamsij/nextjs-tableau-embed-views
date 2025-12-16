f5a5fde9b55277aabb70f2e13519c92bbebba084
kvamse <kvamsij@gmail.com>
2025-12-16 18:33:26 +0000

    fix: reorder import statements in generate-token utility

--- actions/utils/generate-token.ts
+++ actions/utils/generate-token.ts
@@ -1,6 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
 import CryptoJS from 'crypto-js';
-import { TableauUserGroupService } from '@/lib/services/tableau-user-group-service';
+import { v4 as uuidv4 } from 'uuid';
 
 
 
